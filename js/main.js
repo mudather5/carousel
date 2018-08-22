6b8b3748d6cd12ad4aa4ef3f59b3f92b5a8e80d5
mudather5 <mudathermohamed50@gmail.com>
2018-08-22 16:16:56 +0200

    i've added codes in main.css, index.html and main.js

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var first = document.calcul.value
+
+
+function botao() {
+
+
+    document.visor.calcul.value = "7";
+}
+
+
+
+function add(value1, value2){
+    return value1 + value2;
+}
+
+function division(value1, value2){
+    return value1/value2
+}
+
+function mutiplication(value1, value2){
+     return value1 * value2
+}
+
+function subtraction(value1, value2){
+     return value1 - value2
+}
