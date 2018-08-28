2c2fbd2a953728e037ba4b7091898977f08b6d04
mudather5 <mudathermohamed50@gmail.com>
2018-08-28 10:34:37 +0200

    added pictures and some code in main.js and main.html

--- js/main.js
+++ js/main.js
@@ -1,26 +1,52 @@
-var first = document.calcul.value
+/*var slider = document.getElementsByClassName("pictures");
+const INDEXMAX = slider.length - 1;
+
+var i = 0;
 
 
-function botao() {
-
-
-    document.visor.calcul.value = "7";
+for (var i = 0; i < slider.length; i++) {
+    slider[i].style.display = "none";
 }
 
 
+slider[0].style.display = "block";
 
-function add(value1, value2){
-    return value1 + value2;
+
+var myInterval = setInterval(carousel, 2000)
+
+let index = 0;
+
+
+function carousel(n) {
+
+    slider[index].style.display = "block";
+    index++;
+    if (index > INDEXMAX) {
+        index = 0;
+    }
+}*/
+
+
+var i = 0;
+var images = ["img/4.jpg", "img/5.jpg", "img/6.jpg", "img/1.jpeg"];
+var time = 3000;
+
+//function for lincking html and main.js, with changing the pictures
+
+function changImg(){
+    document.slide.src = images[i];
+    if (i < images.length -1){
+        i++;
+    }else{
+        i = 0;
+    }
+    setTimeout("changImg()", time);// the opration for changing the time
 }
 
-function division(value1, value2){
-    return value1/value2
-}
+changImg();
 
-function mutiplication(value1, value2){
-     return value1 * value2
-}
+// function for th button
 
-function subtraction(value1, value2){
-     return value1 - value2
+function plus(n){
+    changImg(index += n);
 }
--- js/plugins.js
+++ js/plugins.js
@@ -1,5 +1,103 @@
+
+var numbers = document.getElementsByClassName("numbers");
+var operators = document.getElementsByClassName('operators');
+var del = document.getElementById("delet");
+var display = document.getElementById('write');
+var equal = document.getElementById("equal");
+var result = "";
+
+for (var i = 0; i < numbers.length; i++){
+  numbers[i].onclick = function(){
+    getValue(this);
+  };
+}
+
+for (var i = 0; i < operators.length; i++){
+  operators[i].onclick = function(){
+    getValue(this);
+  };
+}
+
+
+for (var i = 0; i < equal.length; i++){
+    equal[i].onclick = function(){
+        getValue(this);
+    };
+}
+for (var i = 0; i <  del.length; i++){
+   del[i].onclick = function(){
+       getValue(this);
+   } 
+}
+
+
+function getValue(element){
+  result += element.innerHTML;
+  display.innerHTML = result;
+}
+
+
+/*function opration(element) {
+  if(equal === ){
+     document.getElementById("equal") = eval(equal);
+     }
+}*/
+
+
+function clean(){
+   del.onclick = getValue;
+}
+
+/*function calculate(element){
+    if(element.innerHTML == "+"){
+       equal = numbers + numbers;
+       }else if(element.innerHTML == "-"){
+          equal =  numbers - numbers;
+       }else if(element.innerHTML == "÷"){
+          equal =  numbers ÷ numbers;
+       }else if(element.innerHTML =="*"){
+           equal = numbers * numbers;
+       }else if(element.innerHTML == "%"){
+           equal = numbers ÷ numbers * 100;
+           numbers * 100;
+       }
+}calculate();*/
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Avoid `console` errors in browsers that lack a console.
-(function() {
+/*(function() {
   var method;
   var noop = function () {};
   var methods = [
@@ -21,4 +119,6 @@
   }
 }());
 
-// Place any jQuery/helper plugins in here.
+// Place any jQuery/helper plugins in here.*/
+
+
